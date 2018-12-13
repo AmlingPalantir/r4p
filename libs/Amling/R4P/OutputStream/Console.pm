7659507844119d4edfcfea9e10fa3f30268abdde
Keith Amling <keith.amling@gmail.com>
2018-12-12 22:17:37 -0800

    Give up on Lines OS

--- libs/Amling/R4P/OutputStream/Console.pm
+++ libs/Amling/R4P/OutputStream/Console.pm
@@ -3,9 +3,24 @@ package Amling::R4P::OutputStream::Console;
 use strict;
 use warnings;
 
-use Amling::R4P::OutputStream::Lines;
+use JSON;
 
-use base ('Amling::R4P::OutputStream::Lines');
+my $json = JSON->new();
+
+sub new
+{
+    my $class = shift;
+
+    my $this = {};
+
+    bless $this, $class;
+
+    return $this;
+}
+
+sub write_bof
+{
+}
 
 sub write_line
 {
@@ -13,6 +28,18 @@ sub write_line
     my $line = shift;
 
     print "$line\n";
+}
+
+sub write_record
+{
+    my $this = shift;
+    my $r = shift;
+
+    return $this->write_line($json->encode($r));
+}
+
+sub close
+{
 }
 
 1;
